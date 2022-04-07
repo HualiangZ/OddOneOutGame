551e73cb25b2ee0e3e2acf370d7e0ee7cfed8a4e
HualiangZ <zhualiang88@gmail.com>
2022-04-07 22:19:05 +0100

    loop through all player
    loops through all players and stop changing scene when reached end of list.
    
    Haven't added a way to tell the players if they have the animal or an imposter yet.

--- OddOneOut/Assets/Scenes/GamePlay.cs
+++ OddOneOut/Assets/Scenes/GamePlay.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GamePlay : MonoBehaviour
 {   
     public Text changingText;
    // public AddPlayer player; 
-    public int x = 0;
+    static public int x = 0;
     static public List<string> players = new List<string>();
 
     //use the list to output the players name in to the textfield called 'PlayerNametxt' in 'HideAnimalScene'
@@ -19,10 +21,28 @@ public class GamePlay : MonoBehaviour
 
     //}
 
-    public void textChange()    
+    void Awake()    
     {
-        changingText.text = players[1];
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (players.Count != 0 && sceneName == "HideAnimalScene" )
+            changingText.text = players[x];
  
+    }
+
+    public void ChangeScene()
+    {
+        if(EventSystem.current.currentSelectedGameObject.name == "ShowAnimal")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+        }
+        else if (EventSystem.current.currentSelectedGameObject.name == "Continue" && x < players.Count-1)
+        {
+            x = x + 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        }
+            
     }
 
 
