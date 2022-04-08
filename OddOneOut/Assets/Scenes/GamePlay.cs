e9f9685bde04c52c20d43e1a8ed40c12b8260123
HualiangZ <zhualiang88@gmail.com>
2022-04-08 11:36:23 +0100

    Select imposter
    the code is able to select an imposter and a random animal.
    
    imposter is always the first player unless it randomly generated the first player not sure

--- OddOneOut/Assets/Scenes/GamePlay.cs
+++ OddOneOut/Assets/Scenes/GamePlay.cs
@@ -8,27 +8,47 @@ using UnityEngine.EventSystems;
 public class GamePlay : MonoBehaviour
 {   
     public Text changingText;
-   // public AddPlayer player; 
+    public Text impos;
+
     static public int x = 0;
     static public List<string> players = new List<string>();
-
+    static public List<string> animals = new List<string>() { "Mouse", "Cat", "Dog", "Lion"};
+    static public bool con = false;
+    static public int imposter;
+    static public int animal;
     //use the list to output the players name in to the textfield called 'PlayerNametxt' in 'HideAnimalScene'
 
-    //IEnumerator Start()
-    //{
-    //    player = GetComponent<AddPlayer>();
-    //    yield return new WaitForEndOfFrame();
-
-    //}
 
     void Awake()    
     {
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
+        while(con == false)
+        {
+            imposter = Random.Range(0, players.Count);
+            animal = Random.Range(0, animals.Count);
+            con = true;
+        }
+            
 
         if (players.Count != 0 && sceneName == "HideAnimalScene" )
-            changingText.text = players[x];
- 
+            changingText.text = players[x]; 
+        
+        if(sceneName == "ShowAimalScene")
+        {
+            
+            
+            if(imposter == x)
+            {
+                impos.text = "You are the imposter.";
+            }
+            else
+            {
+                impos.text = "The Animal Is " + animals[animal];
+            }
+
+        }
+
     }
 
     public void ChangeScene()
