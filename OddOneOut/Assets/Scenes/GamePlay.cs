39c12687ab77d22ef01f2a79abba11f057286984
HualiangZ <zhualiang88@gmail.com>
2022-04-17 15:57:43 +0100

    Added voting and display real imposter
    Right now the game is using inputfields to add votes

--- OddOneOut/Assets/Scenes/GamePlay.cs
+++ OddOneOut/Assets/Scenes/GamePlay.cs
@@ -5,42 +5,87 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
+public class players
+{
+    string name;
+    int vote;
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public void setName(string value)
+    {
+        name = value;
+    }
+
+
+    public int getVote()
+    {
+        return vote;
+    }
+
+    public players(string name)
+    {
+        this.name = name;
+        this.vote = 0;
+    }
+
+    public void addVote()
+    {
+        vote += 1; 
+    }
+} 
+
+
+
+
+
 public class GamePlay : MonoBehaviour
 {   
     public Text changingText;
-    public Text impos;
+    public Text impos;//imposter text
+    public Text v; //the most vote player text
+    public Text voting;//who is voting text
 
-    static public int x = 0;
-    static public List<string> players = new List<string>();
+
+    static public int x = 0;//to keep track of players in showing animal 
+    static public int y = 0;//keep track players on who is voting
+    static public List<players> p = new List<players>(); 
     static public List<string> animals = new List<string>() { "Mouse", "Cat", "Dog", "Lion"};
     static public bool con = false;
-    static public int imposter;
-    static public int animal;
+    static public int imposter;// rng imposter on number gen
+    static public int animal;//rng animal on number gen
     //use the list to output the players name in to the textfield called 'PlayerNametxt' in 'HideAnimalScene'
 
 
     //public InputField mainInputField;
 
-    void Awake()    
+    void Awake()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
-        while(con == false)
+        //generate randome animal and imposter 
+        while (con == false)
         {
-            imposter = Random.Range(0, players.Count);
+            imposter = Random.Range(0, p.Count);
             animal = Random.Range(0, animals.Count);
             con = true;
         }
-            
 
-        if (players.Count != 0 && sceneName == "HideAnimalScene" )
-            changingText.text = players[x]; 
-        
-        if(sceneName == "ShowAimalScene")
+        //making sure this is the right player to show if they ar animal or imposter
+        if (p.Count != 0 && sceneName == "HideAnimalScene")
+            //string player = 
+            changingText.text = "Is This " + p[x].getName();
+
+
+        //show the player if they are animal or imposter
+        if (sceneName == "ShowAimalScene")
         {
-            
-            
-            if(imposter == x)
+
+
+            if (imposter == x)
             {
                 impos.text = "You are the imposter.";
             }
@@ -51,39 +96,92 @@ public class GamePlay : MonoBehaviour
 
         }
 
+        //chech who got the most vote and display it and display the real imposter
+        if (sceneName == "MostPlayerVoteScene")
+        {
+            int x = 0;
+            int player = 0;
+            for (int i = 0; i < p.Count; i++)
+            {
+                if(x < p[i].getVote())
+                {
+                    player = i;
+                }
+
+            }
+
+            v.text = "You Voted For " + p[player].getName();
+            impos.text = "The Real Imposter Is " + p[imposter].getName();
+        }
+
+
+        //tell who is voting
+        if (sceneName == "VoteScene")
+        {
+            voting.text = p[y].getName();
+        }
+
     }
 
+    //add vote to player.
+    public void Vote(string s)
+    {
+        for(int i=0; i < p.Count; i++)
+        {
+            if(p[i].getName() == s)
+            {
+                p[i].addVote();
+            }
+        }
+    }
+
+    //check if all players have voted
+    public void ChangePlayerVote()
+    {
+        if (EventSystem.current.currentSelectedGameObject.name == "Vote" && y < p.Count - 1)
+        {
+            y++;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(7);
+        }
+       
+    }
+
+    //change Scene when all players have shown their role
     public void ChangeScene()
     {
         if(EventSystem.current.currentSelectedGameObject.name == "ShowAnimal")
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(3);
         }
-        else if (EventSystem.current.currentSelectedGameObject.name == "Continue" && x < players.Count-1)
+        else if (EventSystem.current.currentSelectedGameObject.name == "Continue" && x < p.Count-1)
         {
             x = x + 1;
             UnityEngine.SceneManagement.SceneManager.LoadScene(2);
-        }else if (EventSystem.current.currentSelectedGameObject.name == "Continue" && x == players.Count - 1)
+        }else if (EventSystem.current.currentSelectedGameObject.name == "Continue" && x == p.Count - 1)
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(5);
         }
             
     }
 
-    //comment
-    //comment
+    //add player name to list
     public void ReadStringInput(string s)
     {
         //mainInputField.DeactivateInputField();
-        players.Add(s); //new comment
+
+        p.Add(new players(s));
 
         //used for testing
         //------------------------------
-        foreach (string p in players)
+        for (int i = 0; i<p.Count; i++)
         {
-            Debug.Log(p);
+            Debug.Log(p[i].getName());
         }
-        int x = players.Count;
+        int x = p.Count;
 
 
         Debug.Log(x);
